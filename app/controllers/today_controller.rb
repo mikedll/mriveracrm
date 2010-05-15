5725806c96eba94527a1160476cd4e44efa75e74
Michael Rivera <michaelrivera@osxmike.local>
2010-05-15 22:33:50 +0530

    Support for both today and yesterday. Using YAML for loading up more info about todos.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,12 +2,29 @@ class TodayController < ApplicationController
   layout 'home'
   
   def show
-    t = Time.zone.now
-    srand( t.day * 100 + t.mon * 1000 )
-    b = ["bio/physics", "math/stats/algorithm", "fitness & presentation", "dragon development", "service 1", "service 2"]
-    a = []
-    100.times { a << ((rand() * 100).to_i % 6) }
-    @tasks = a.uniq.map { |i| b[i] }
+    b = YAML.load <<-TASKS
+      - 3 math/stats/algorithm training
+      - 2 bio/physics training
+      - 1 fitness & presentation
+      - 2 dragon development (including quagress input and development)
+      - 3 service 1
+      - 3 service 2
+      - 1 hr breakfast / cleaning clothes / shower
+      - .5 shopping (food, other)
+      - .5 hour internet reading
+      - .5 write to mel/amy/peter, or apply for a job somewhere
+      - .5 blog writing & publishing
+    TASKS
+    
+    @tasks = random_indicies.map { |i| b[i] }
+    @old_tasks = random_indicies(Time.zone.now - 1.day).map { |i| b[i] }
   end
 
+
+  def random_indicies(t = Time.zone.now)
+    a = []
+    srand( t.day * 100 + t.mon * 1000 )
+    100.times { a << ((rand() * 100).to_i % 6) }    
+    a.uniq
+  end
 end
