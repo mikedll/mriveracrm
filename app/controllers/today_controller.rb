1026f96688e581a21683a32ce7c8a3ae5405dd34
Michael Rivera <michaelrivera@osxmike.local>
2010-05-15 22:40:39 +0530

    Increased number of elements in todo list.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,7 +2,7 @@ class TodayController < ApplicationController
   layout 'home'
   
   def show
-    b = YAML.load <<-TASKS
+    a = YAML.load <<-TASKS
       - 3 math/stats/algorithm training
       - 2 bio/physics training
       - 1 fitness & presentation
@@ -10,21 +10,23 @@ class TodayController < ApplicationController
       - 3 service 1
       - 3 service 2
       - 1 hr breakfast / cleaning clothes / shower
-      - .5 shopping (food, other)
-      - .5 hour internet reading
-      - .5 write to mel/amy/peter, or apply for a job somewhere
-      - .5 blog writing & publishing
+      - 0.5 shopping (food, other)
+      - 0.5 hour internet reading
+      - 0.5 write to mel/amy/peter/andrew, or apply for a job somewhere
+      - 0.5 blog writing & publishing
     TASKS
-    
-    @tasks = random_indicies.map { |i| b[i] }
-    @old_tasks = random_indicies(Time.zone.now - 1.day).map { |i| b[i] }
+    @tasks = randtasks(a)
+    @old_tasks = randtasks(a, Time.zone.now - 1.day)
   end
 
+  def randtasks(a,t = Time.zone.now)
+    random_indicies(a.size, t).map { |i| a[i] }
+  end
 
-  def random_indicies(t = Time.zone.now)
+  def random_indicies(n, t)
     a = []
     srand( t.day * 100 + t.mon * 1000 )
-    100.times { a << ((rand() * 100).to_i % 6) }    
+    100.times { a << ((rand() * 100).to_i % n) }    
     a.uniq
   end
 end
