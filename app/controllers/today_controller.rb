5a5bbcd92f7e3d9da76650d12b6eed153e1693cf
Michael Rivera <michaelrivera@osxmike.local>
2010-12-15 00:04:00 +0530

    Changing priorities.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,24 +2,25 @@ class TodayController < ApplicationController
   layout 'home'
   
   def show
+
     a = YAML.load <<-TASKS
-    - 7 Hours Sleep
-    - 5 bcg service
-    - 1 dragon development (emacs, personal tool development, portfolio enhancements, daily budgeting and planning, automation of daily activities like mikedll/today, base, publishing platform development, quagress input)
-    - 1 quagress service
+    - 8 Hours Sleep
+    - 1 hr getdonkies.com
+    - 1 quagress
+    - 1 Bomberman/altitude project
+    - 1 Nochipmonks
+    - 1 hr PimpEQ
+    - 1 fitness & presentation, drawing (paper and photoshop)
     - 3 hr math exercises, with some publishing.
-    - 2 hr math tool development
-    - 0.5 more of fitness & presentation, drawing (paper and photoshop)
-    - 1.25 going out to eat
-    - 1 cooking, cleaning
-    - 1.25 transportation
-    - 1 apply for a job. make a letter. write. publish something. 
+    - 1 dragon development (emacs, personal tool development, portfolio enhancements, daily budgeting and planning, automation of daily activities like mikedll/today, base, publishing platform development, quagress input)
+    - 1 eating / shopping
+    - 5 apply/advertise for a job/room. make a letter. write. publish something. 
     TASKS
+
     @tasks = randtasks(a)
     @total = total( randtasks(a) )
     @old_tasks = randtasks(a, Time.zone.now - 1.day)
   end
-
   def total( tasks )
     tasks.inject(0) { |acc,t| t =~ /^(\d(\.\d+)?)/; acc += $1.to_f }
   end
