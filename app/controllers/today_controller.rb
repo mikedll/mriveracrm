8003fe813f217e88430bdd0d49b47a0931b4f9f6
Michael Rivera <michaelrivera@osxmike.local>
2010-12-26 12:40:09 -0800

    Adjusting tasks.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -2,25 +2,23 @@ class TodayController < ApplicationController
   layout 'home'
   
   def show
-
     a = YAML.load <<-TASKS
     - 8 Hours Sleep
-    - 1 hr getdonkies.com
-    - 1 quagress
-    - 1 Bomberman/altitude project
-    - 1 Nochipmonks
-    - 1 hr PimpEQ
-    - 1 fitness & presentation, drawing (paper and photoshop)
-    - 3 hr math exercises, with some publishing.
+    - 5 client 1 service
+    - 5 client 2 service
     - 1 dragon development (emacs, personal tool development, portfolio enhancements, daily budgeting and planning, automation of daily activities like mikedll/today, base, publishing platform development, quagress input)
-    - 1 eating / shopping
-    - 5 apply/advertise for a job/room. make a letter. write. publish something. 
+    - 1 quagress|getadonkey|pimpeq|pybomber service
+    - 1 hr math exercises, with some publishing.
+    - 1.5 going for a walk or to eat
+    - 0.5 more of fitness & presentation, drawing (paper and photoshop)
+    - 0.5 apply for a job. make a letter. write. publish something.
     TASKS
 
     @tasks = randtasks(a)
     @total = total( randtasks(a) )
     @old_tasks = randtasks(a, Time.zone.now - 1.day)
   end
+
   def total( tasks )
     tasks.inject(0) { |acc,t| t =~ /^(\d(\.\d+)?)/; acc += $1.to_f }
   end
