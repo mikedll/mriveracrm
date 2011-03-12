62c3dd8bf7cb61a0b44816ebaba492fd0a1eab41
Mike De La Loza <mikedll@mikedll.com>
2011-03-11 18:25:29 -0800

    Working version of highlighter, which seems less gay than SH.

--- public/javascripts/syntaxHighlighterConfig.js
+++ public/javascripts/syntaxHighlighterConfig.js
@@ -1,35 +1,23 @@
 /**
  * Requires jquery to be loaded.
  */
-
-
 $(function() {
-
-      function path()
-      {
-          var args = arguments, result = [];
-
-          for(var i = 0; i < args.length; i++)
-              result.push(args[i].replace('@', 'http://alexgorbatchev.com/pub/sh/3.0.83/scripts/'));
-          
-          return result;
-      };
-      
-      alert(path(
-                'bash shell @shBrushBash.js'
-            )
-           );
-
-      SyntaxHighlighter.autoloader.apply(null, 
-                                         path(
-                                             'bash shell @shBrushBash.js'
-                                         )
-                                        );
-
-      
-      SyntaxHighlighter.config.bloggerMode=true;
-      SyntaxHighlighter.config.tagName = 'code';
-      SyntaxHighlighter.all();
+    $(document).ready(function() {
+                          var fixOld = function(el) {
+                              var node = $(el);
+                              var matches = node.attr('class').match(/^brush: (\w+); gutter: false;?$/);
+                              if( matches !== null ) {                                  
+                                  var lang = matches[1];
+                                  node.attr('class', lang);
+                                  console.debug(lang);
+                              }
+                              return node.first().get()[0];
+                          };
+                          $('pre code').each(function(i, e) { 
+                                                 
+                                                 hljs.highlightBlock(fixOld(e), '  '); 
+                                             });
+    });
   });
 
 
