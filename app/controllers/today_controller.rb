daf863b94f4b2bfeb672c2b76684899ac3a282c1
Mike De La Loza <mikedll@mikedll.com>
2011-04-14 21:14:41 -0700

    Mostly pointless updates to tasks.

--- app/controllers/today_controller.rb
+++ app/controllers/today_controller.rb
@@ -3,19 +3,59 @@ class TodayController < ApplicationController
   
   def show
     a = YAML.load <<-TASKS
+  10 Regular: 
     - 7 Hours Sleep
-    - 1 quagress|getadonkey|pimpeq|pybomber service
-    - 5 client 1 service
-    - 5 client 2 service
+
+    - 1 maint, clean, shower, shopping
+
+    - 1 fitness & presentation, drawing (weights)
+
+    - 1 fitness & presentation, drawing (cardio)
+
+  10 Rotating work:
+
+    - 4 service for plovertech
+
+    - 5 service for jamie and spencer
+
+  4 Rotating other
+
+    - 2 quagress|bustadonkey|pimpeq|pybomber service
+
+    - 1 (Science & Medicine, Math, Stats and Engineering) bio exercise
+
+    - 0.5 publish something.
+
+    - 1 play with new tech (node.js, and publish)
+
+    - 1 hr math exercises, with some publishing.
+
+    - 0.5 tutoring/stack overflow/math answering
+
+    - 1 socialize / penpal / write
+
     - 1 dragon development (emacs, personal tool development,
       portfolio enhancements, daily budgeting and planning, automation
       of daily activities like mikedll/today, base, publishing
       platform development, quagress input)
-    - 1 hr math exercises, with some publishing.
-    - 2 going for a walk or to eat
+
     - 1 fitness & presentation, drawing (paper and photoshop)
-    - 0.5 publish something.
-    - 0.5 tutoring/stack overflow/math answering
+
+    - 1 (Math, Stats and Engineering) physics exercise
+
+    - 1 fitness & presentation, drawing (music production)
+
+    - 1 fitness & presenttiation (voice)
+
+    - 0.5 language learning; hindi
+
+    - 0.5 stats exercises
+
+    - 0.5 (stats & enginerring) physics exercises
+
+    - 0.5 programming / algorithm competition
+
+    - 0.5 internet reading
     TASKS
 
     @tasks = randtasks(a)
