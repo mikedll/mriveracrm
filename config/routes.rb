9e79507308c80c8f9b8386060ae54cba9bc8a876
Mike De La Loza <mikedll@mikedll.com>
2012-04-04 14:07:01 -0700

    Middle of updates.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Mikedll::Application.routes.draw do
 
   get "stream", :to => 'home#stream', :as => :stream
 
+  resource :home, :controller => "home", :path => "", :only => [] do
+    get :projects
+  end
+
   root :to => "home#index"
 
 end
