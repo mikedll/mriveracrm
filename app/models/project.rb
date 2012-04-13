83c75972762353fc9dc985aa83ba65aaa7213213
Mike De La Loza <mikedll@mikedll.com>
2012-04-13 00:46:45 -0700

    Description updates.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,13 +7,14 @@ class Project
               {
                 :title => "SnapKnot.com",
                 :image => '1.jpg',
-                :desc => "SnapKnot.com is a directory of wedding photographers. Photographers can create profiles and portfolios that demonstrate their work. Brides and other leads can search for photographers by city and price range. My role was to optimize database performance, implement a web service for the iPhone app to talk to, and to get spatial searches to work with latitude and longtitude coordinates. Mike and Reid, the site's cofounders, were great guys to work with.",
-                :tech => 'Ruby on Rails, Amazon S3, MySQL, Ubuntu Linux, Linode'
+                :desc => "SnapKnot.com is a directory of thousands of wedding photographers. Wedding Photographers can signup and create portfolios that showcase their work. Brides can search for photographers by city and price range. My job was to optimize database performance, to integrate with 3rd party payment gateways and photohosting sites, to implement a web service for the iPhone app, and to get geospatial searches to work with latitude and longtitude coordinates.",
+                :tech => 'Ruby on Rails, jQuery, Amazon S3, MySQL, Ubuntu Linux, Linode'
               },
+
               {
                 :title => "MyLearningPlan.com",
                 :image => '2.jpg',
-                :desc => 'My Learning Plan is a massive data processing automation app. It serves millions of school districts in the United States. Most K-12 teachers need to do professional development for a variety of reasons - salary increases or state legal requirements - and this app tracks that information in a centralized fashion for school districts. My job has been to port a ton of legacy classic ASP code to the Microsoft .NET 4.0 framework, which including a lot of interopability work.',
+                :desc => 'My Learning Plan is a massive form automation app. It serves millions of school districts in the United States. Most K-12 teachers need to do professional development for a variety of reasons - salary increases or legal requirements - and this app tracks their progress for their school districts. My job has been to port tens of thousands of lines of classic ASP to C# on the Microsoft .NET 4.0 framework.',
                 :tech => 'Microsoft .NET, IIS, Classic ASP and ASP.NET, COM+, MVC3, ExtJS'
               }
              ]
