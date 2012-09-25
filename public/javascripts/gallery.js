586725a00f7fd60b70b393ff67d3f1270f4f834c
Mike De La Loza <mikedll@mikedll.com>
2012-09-25 10:00:27 -0700

    Key navigation around the gallery.

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -1,6 +1,118 @@
+
+/**
+ * Google Blogger API
+ * http://code.google.com/apis/blogger/docs/1.0/developers_guide_js.html
+ */
+
+google.load("feeds", "1");
+
+function results_callback(results) {
+  if (!results.error) {
+  	for (var i = 0, entry; entry = results.feed.entries[i]; i++) {
+  	    var d = new Date( entry.publishedDate );
+  	    var n = '<li><a href=":href">:title</a> <span class="date">:date</span></li>'
+		.replace( /:href/, entry.link )
+		.replace( /:title/, entry.title )
+		.replace( /:date/, prettyDate( d ) );
+
+  	    $('#blog_entries').append( n );
+  	}
+  }
+}
+function initialize() {
+    var feed = new google.feeds.Feed("http://blog.mikedll.com/feeds/posts/default");
+
+    feed.setNumEntries( 4 );
+    feed.load( results_callback );
+}
+
+$(initialize);
+
+var STATES = {
+    MAIN: 0,
+    PROJECTS: 1,
+    GALLERY: 2
+};
+
+var GLOBAL_STATE = STATES.MAIN;
+
+function setupGallery() {
+
+    var transition = function( out, into, new_state ) {
+	      out.fadeOut( 200, function() { 
+                         into.fadeIn( 700 ); 
+                         GLOBAL_STATE = new_state;
+                     } );
+    };
+
+    var homeToProjects = function() {
+	      transition( $('#main'), $('.pg_content').add('#thumbContainter'), STATES.PROJECTS );
+    };
+
+    var projectsToHome = function() {
+        $('#overlay').fadeOut(200);
+	      transition( $('.pg_content').add('#thumbContainter'), $('#main'), STATES.MAIN );
+    };
+
+    $('#projects_link').bind('click', homeToProjects );
+
+    $('#thumbContainter').add('.pg_content')
+        .bind('click', function(e) {
+                         e.stopPropagation();
+                     });
+
+
+    $(document)
+        .bind('click', function() {
+                         if(GLOBAL_STATE == STATES.PROJECTS)  {
+                             projectsToHome();
+                         }
+              })
+        .bind('keyup', function(e) {
+                  //
+                  // This is ugly because it depends on a lot of selectors that are repeated in the below
+                  // gallery setup. Such is what happens when you merge disparate pieces of code.
+                  //
+                  if (GLOBAL_STATE == STATES.PROJECTS) {
+                      if(e.keyCode === 38 || e.keyCode === 40) {
+                          e.stopPropagation();
+                          var allThumbnails = $('#thumbScroller .content');
+                          var nextIndex = -1;
+                          console.log( $('.pg_content').data('current-project') );
+                          if( e.keyCode === 38 && $('.pg_content').data('current-project') > 0) {
+                              nextIndex = $('.pg_content').data('current-project') - 1;
+                          }
+                          else if (e.keyCode === 40 && $('.pg_content').data('current-project')  < allThumbnails.length - 1) {
+                              nextIndex = $('.pg_content').data('current-project') + 1;
+                          }
+
+                          if(nextIndex !== -1 ) {
+                              allThumbnails.eq( nextIndex ).trigger('click');
+                          }
+                      }
+                      else if(e.keyCode === 27) {
+                          e.stopPropagation();
+                          projectsToHome();
+                      }
+                      else if(e.keyCode === 13) {
+                          e.stopPropagation();
+                          if( $('#pg_large').length === 0) {
+                              $('#pg_preview .pg_thumb_block').eq( $('.pg_content').data('current-project') )
+                                  .find('.pg_thumb').first().trigger('click');
+                          }
+                      }
+                  }
+
+              });
+
+}
+$(setupGallery);
+
+
+
 $(function() {
 			//index of current item
-			var current				= 0;
+      $('.pg_content').data('current-project', 0);
 			//speeds / ease type for animations
 			var fadeSpeed			= 400;
 			var animSpeed			= 600;
@@ -22,6 +134,14 @@ $(function() {
 			var totalContent		= 0;
 			//one items height
 			var itemHeight			= 0;
+
+      var mostRecentStartPosition = {
+              'top': '0px',
+              'left': '250px',
+              'width': '360px',
+              'height': '300px'
+          };
+
 			
 			//First let's create the scrollable container,
 			//after all its images are loaded
@@ -49,7 +169,7 @@ $(function() {
 					                      
 					                      var idx 				= $this.index();
 					                      //if we click on the one shown then return
-					                      if(current==idx) return;
+					                      if($('.pg_content').data('current-project')==idx) return;
 					                      
 					                      //if the current image is enlarged,
 					                      //then we will remove it but before
@@ -62,6 +182,7 @@ $(function() {
 					                      }
 					                      
 					                      //get the current and clicked items elements
+                                var current = $('.pg_content').data('current-project');
 					                      var $currentTitle 		= $pg_title.find('h1:nth-child('+(current+1)+')');
 					                      var $nextTitle 			= $pg_title.find('h1:nth-child('+(idx+1)+')');
 					                      var $currentThumb		= $pg_preview.find('.pg_thumb_block:eq('+current+')');
@@ -72,7 +193,7 @@ $(function() {
 					                      var $nextDesc2 			= $pg_desc2.find('div:nth-child('+(idx+1)+')');
 					                      
 					                      //the new current is now the index of the clicked scrollable item
-					                      current		 			= idx;
+					                      $('.pg_content').data('current-project', idx);
 					                      
 					                      //animate the current title up,
 					                      //hide it, and animate the next one down
@@ -113,7 +234,51 @@ $(function() {
 			//the thumb. After that we hide the thumb so that
 			//the large one gets displayed
 			$pg_preview.find('.pg_thumb').bind('click',showLargeImage);
-			
+
+
+      function unzoomGallery(){
+          $overlay.fadeOut(200);
+          var $largeImg = $('#pg_large');
+          var $thumb = $pg_preview.find('.pg_thumb').eq($largeImg.data('thumb-index'));
+          $largeImg.stop().animate(mostRecentStartPosition,500, function() {
+                                       $largeImg.remove();
+                                       $thumb
+                                           .css({'z-index': 9999});
+                                       
+			                                 $pg_preview.find('.pg_thumb').bind('click', showLargeImage);
+                                       GLOBAL_STATE = STATES.PROJECTS;
+                                   });
+      }
+	    
+      function keyCatcher(e) {
+          if( e.keyCode === 39 || e.keyCode === 37) {
+              e.stopPropagation();              
+              var thumbs = $('#pg_preview .pg_thumb_block').eq( $('.pg_content').data('current-project') ).find('.pg_thumb');
+              var large = $('#pg_large');
+              var nextIndex = -1, nextThumb;
+
+              if( e.keyCode === 39 && large.data('thumb-index') < thumbs.length - 1 ) {
+                  nextIndex = large.data('thumb-index') + 1;
+              }
+              else if (e.keyCode === 37 && large.data('thumb-index') > 0 ) {
+                  nextIndex = large.data('thumb-index') - 1;                  
+              }
+
+              if(nextIndex !== -1 ) {
+                  nextThumb = thumbs.eq( nextIndex );
+                  large
+                      .attr('src', nextThumb.attr('alt'))
+                      .data('thumb-index', nextIndex);                  
+              }
+          }
+          else if (e.keyCode === 27) {
+              unzoomGallery();
+              e.stopPropagation();
+          }
+      }
+
+      $(document).bind('keyup', keyCatcher);
+
 			//enlarges the thumb
       function showLargeImage(){
           //if theres a large one remove
@@ -129,7 +294,7 @@ $(function() {
           var padding = parseInt($thumb.css('padding'));
           var left = padding + ($thumb.index() * (padding * 2)) + ($thumb.index() * parseInt($thumb.css('width'))); // leftmost padding, plus padding inbetween elements (collapsed), plus 
           var top = padding;
-          var startPosition = {
+          mostRecentStartPosition = {
               'top': top,
               'left': left,
               'width': '360px',
@@ -143,37 +308,25 @@ $(function() {
           };
 
           // grow image
-          var $growerImg = $('<img id="grower"/>').css(startPosition);
+          var $growerImg = $('<img id="grower"/>').css(mostRecentStartPosition);
           $growerImg.attr('src', $thumb.attr('src'));
           $growerImg.insertBefore($thumb);
           $growerImg.animate(endPosition,
                              500,
                              function() {
-                                 
 							                   $thumb.css({
 								                                'opacity'	: 1,
 								                                'z-index'	: 1
 							                              });
-
-                                 $('<img src="' + large_src + '" id="pg_large"/>').css(endPosition).load(function() {
-                                                                                            var $largeImg = $(this);
-                                                                                            $largeImg.insertAfter($thumb).show();
-                                                                                            $growerImg.remove();
-							                                                                              $largeImg.bind('click',function(){
-                                                                                                               // $thumb.show();
-                                                                                                               $overlay.fadeOut(200);
-                                                                                                               $(this).stop().animate(startPosition,500, function() {
-                                                                                                                                          $largeImg.remove();
-                                                                                                                                          $thumb
-                                                                                                                                              .css({'z-index': 9999});
-
-			                                                                                                                                    $pg_preview.find('.pg_thumb').bind('click', showLargeImage);
-                                                                                                                                      });
-                                                                                                           });
-                                                                                            
-                                                                                        });
-                             });
-          
+                                 
+                                 $('<img src="' + large_src + '" id="pg_large" data-thumb-index="' + $thumb.index() + '"/>').css(endPosition).load(function() {
+                                                                                                             var $largeImg = $(this);
+                                                                                                             $largeImg.insertAfter($thumb).show(); // the positioning after the thumb is arbitrary orderwise, since this is absolutely positioned...
+                                                                                                             $growerImg.remove();
+                                                                                                             GLOBAL_STATE = STATES.GALLERY;
+							                                                                                               $largeImg.bind('click', unzoomGallery);
+                                                                                                         });
+                             });          
 			}
 			
 			//resize window event:
