f01819ea3127da25512961c5b47908ae0bc6fb4d
Mike De La Loza <mikedll@mikedll.com>
2012-09-25 15:23:30 -0700

    Fixing asset host for fog/carrier wave uploads.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,5 +12,6 @@ CarrierWave.configure do |config|
   config.fog_directory  = s3store['bucket']
   # config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-  config.asset_host     = 'http://mikedll.com-production.s3.amazonaws.com'            # optional, defaults to nil
+
+  config.fog_host     = "http://#{config.fog_directory}.s3.amazonaws.com"            # optional, defaults to nil
 end
