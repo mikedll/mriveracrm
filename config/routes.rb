6c14c8ebb84144475b02eb4bfeec2b18ca56ba2b
Mike De La Loza <mikedll@mikedll.com>
2013-03-21 05:44:35 -0700

    old livestream removed.

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ MikedllCrm::Application.routes.draw do
 
   get "today", :to => 'today#show', :as => :todays
 
-  get "stream", :to => 'home#stream', :as => :stream
-
   resource :home, :controller => "home", :path => "", :only => [] do
     get :contact
     get :projects
