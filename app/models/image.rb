94dd9ff8e88a6a69a3629c0d337d6e5e1510f44c
Mike De La Loza <mikedll@mikedll.com>
2013-08-07 13:13:04 -0700

    Destroy middle-man project_image records if image or product is destroyed.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
   belongs_to :business
   belongs_to :project
 
-  has_many :product_images
+  has_many :product_images, :dependent => :destroy
   has_many :products, :through => :product_images
 
   mount_uploader :data, ImageUploader
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 
   belongs_to :business
 
-  has_many :product_images
+  has_many :product_images, :dependent => :destroy
   has_many :images, :through => :product_images
 
   attr_accessible :name, :price, :weight, :active, :weight_units, :description
