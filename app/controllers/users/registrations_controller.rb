8e812013e16d65f5ef4600ab5827bef1c3ae2d00
Mike De La Loza <mikedll@mikedll.com>
2014-02-17 17:22:40 -0800

    Bringing in form for new user signup with business handle.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,25 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  skip_before_filter :authenticate_user!
+
   #
   # Need to require a business at the same time,
   # else we can't map the user.
   #
   def create
-    
+    @business = Business.new(:handle => params[:business])
+    Business.transaction do |t|
+      if @business.save
+        @user = User.new(params[:user].merge(:business => @business))
+        if @user.save
+          redirect_to after_sign_up_path(@user)
+        else
+          render "registrations/new"
+        end
+      else
+        render "registrations/new"
+      end
+    end
   end
 
 end
