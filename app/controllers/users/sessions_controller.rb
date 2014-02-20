f11c6c367a1bafaf9588defd5b3815a66a0de048
Mike De La Loza <mikedll@mikedll.com>
2014-02-19 20:38:53 -0800

    Can login with username and password.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
 
-  skip_before_filter :authenticate_user!, :only => [:new, :authorize, :google_oauth2]
-  skip_before_filter :require_business_and_current_user_belongs_to_it, :only => [:new, :authorize, :google_oauth2]
+  skip_before_filter :authenticate_user!, :only => [:new, :create, :authorize, :google_oauth2]
+  skip_before_filter :require_business_and_current_user_belongs_to_it, :only => [:new, :create, :authorize, :google_oauth2]
 
   class NoopApp
     def call(env); end;
