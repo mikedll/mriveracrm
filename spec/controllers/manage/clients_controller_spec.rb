4b430d7316c8d57d16a47dbc16bf234843796326
Mike De La Loza <mikedll@mikedll.com>
2014-03-11 14:30:17 -0700

    Moving clients controller spec under manage subdir.

--- spec/controllers/manage/clients_controller_spec.rb
+++ spec/controllers/manage/clients_controller_spec.rb
@@ -9,14 +9,27 @@ describe Manage::ClientsController do
       get :index
       response.should redirect_to(:host => 'www.mikedll.com')
     end
+
+    it "should asdf" do
+      @mfe = FactoryGirl.create(:marketing_front_end)
+      @user = FactoryGirl.create(:employee_user)
+      sign_in @user
+
+      request.host = @mfe.host # "www.test.com" # @mfe.host
+      # request.host = @user.employee.business.host
+
+      get :index
+      response.should redirect_to(bhandle_manage_clients_path(:business_handle => @user.employee.business.handle))
+    end
+
   end
 
-  context "routine use" do
+  context "routine use", :current => true do
     before(:each) do
       Stripe::Customer.stub(:create) { ApiStubs.stripe_create_customer }
       @user = FactoryGirl.create(:employee_user)
       sign_in @user
-      request.host = @user.employee.business.domain
+      request.host = @user.employee.business.host
     end
 
     it "should create clients" do
