f427e2b9bac677fba0f072712a48807f4f74b4ad
Mike De La Loza <mikedll@mikedll.com>
2014-06-19 06:58:49 -0700

    Leave mail delivery off on prod, for now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ MikedllCrm::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'www.mriveracrm.com:80' }
+  config.action_mailer.delivery_method = :test
 
   config.action_controller.asset_host = 'd1dmxqf6tri91s.cloudfront.net'
 
