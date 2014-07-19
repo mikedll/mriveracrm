4a6462bfffc68b854a4aa7abeded6f5c942da049
Mike De La Loza <mikedll@mikedll.com>
2014-07-18 21:10:20 -0700

    case 174 rename release to generation in validations of feature pricing.

--- app/models/feature_pricing.rb
+++ app/models/feature_pricing.rb
@@ -2,7 +2,7 @@ class FeaturePricing < ActiveRecord::Base
   attr_accessible :feature_name, :price, :release, :index
 
   validates :price, :presence => true
-  validates :release, :presence => true
+  validates :generation, :presence => true
   validates :feature_name, :presence => true
   validates :index, :uniqueness => true
 end
