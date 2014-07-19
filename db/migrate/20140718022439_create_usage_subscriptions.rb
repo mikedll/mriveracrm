eeea519e6e89c2bcc3884125ad00dc70884d3b5d
Mike De La Loza <mikedll@mikedll.com>
2014-07-18 19:44:54 -0700

    case 174 on second thought, use merely stripe status for now.

--- db/migrate/20140718022439_create_usage_subscriptions.rb
+++ db/migrate/20140718022439_create_usage_subscriptions.rb
@@ -2,7 +2,6 @@ class CreateUsageSubscriptions < ActiveRecord::Migration
   def up
     create_table :usage_subscriptions do |t|
       t.integer :business_id,   :null => false, :default => 0
-      t.string  :status,        :null => false, :default => ""
       t.string  :card_brand,    :null => false, :default => ""
       t.string  :card_last_4,   :null => false, :default => ""
       t.string  :plan,          :null => false, :default => ""
