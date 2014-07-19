ce5d86524895b3afe1fb17be081f23dc2461397f
Mike De La Loza <mikedll@mikedll.com>
2014-07-19 03:41:34 -0700

    case 174 fix spec demo of pack and base64 encoding.

--- spec/models/usage_subscription_spec.rb
+++ spec/models/usage_subscription_spec.rb
@@ -9,40 +9,42 @@ describe UsageSubscription do
     end
   end
 
-  context "bit string demos" do
+  context "bit string demos", :current => true do
     it "should show ruby bit string demos. (not a real spec)" do
-
       scheme = 1
       generation = 2
-      feature_indicies = [0, 1, 2, 3, 7, 11, 12]
+      feature_indices = [0, 1, 2, 3, 7, 11, 12]
 
       # 4 bits scheme, 4 bits generation, 16 bits features. 24 bits.
       s = "0001" + "0010" + "0001100010001111"
-      bit_array = s.split('') # ["0", "0", "0", "1",   "0", "0", "1", "0",   "0", "0", "0", "1", "0", "0", "0", "1", "0", "0", "0", "1", "1", "1", "1"]
-s.length
-bitstring.length
-      binary_s = bit_array.pack("b" * 24)
+      binary_s = [s].pack("B*") # "\x12\x18\x8F"
+      base64s = [binary_s].pack("m0") # "EhiP"
 
-      binary_s = bit_array.pack("m0m0m0m0")
+      base64s.should == "EhiP"
+      Base64.strict_encode64(binary_s).should == base64s
 
+      decoded_binary_s = Base64.strict_decode64(base64s)
+      decoded_s = decoded_binary_s.unpack("B*").first
+      decoded_s
+      decoded_s[4,4]
 
-      base64_encoded = Base64.strict_encode64(binary_s)
-s = 
-("\x00\x00\x00\x00\x00\x00").length
-Base64.strict_encode64("\x00\x00\x00\x00\x00\x00")
-Base64.strict_encode64("\x01\x04")
-"\x00\x00\x00\x01\\x00x04\x00\x00".length
-["\x00\x00\x00\x01\x00x04\x00\x00"].pack("m0")
+      decoded_scheme = decoded_s[0,4]
+      decoded_generation = decoded_s[4,4]
+      decoded_version_bits = decoded_s[8,decoded_s.length - 8]
 
-sb = "\x00\x00\x00\x01\x00x04\x00\x00".force_encoding("ASCII-8BIT")
-[sb].pack("m")
+      [[decoded_scheme, scheme],
+        [decoded_generation, generation]].each do |pairing|
+        ["0000" + pairing.first].pack("B*").unpack("H*").first.to_i.should == pairing.last
+      end
 
-Base64.strict_decode64('AAAAAQB4MDQAAA==')
-      base64_encoded.length
-s = ['1', '0', '0', '0'].pack("b4b64")
+      decoded_feature_indices = []
+      (0...decoded_version_bits.length).each do |i|
+        if decoded_version_bits[decoded_version_bits.length - 1 - i] == "1"
+          decoded_feature_indices.push(i)
+        end
+      end
 
-    Base64.encode("1010")
-
+      decoded_feature_indices.should =~ feature_indices
     end
   end
 end
