656cfaab82c85de76631b8b6acfeb2d1f4ef1b05
Mike De La Loza <mikedll@mikedll.com>
2014-12-05 05:37:39 -0800

    specs: more realistic invitation accept spec.

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -38,7 +38,7 @@ describe Invitation do
   context "accept" do
     it "should move invitation from available to accepted" do
       invitation = FactoryGirl.create(:client_invitation)
-      user = FactoryGirl.create(:employee_user)
+      user = FactoryGirl.create(:client_user)
       invitation.accept_user!(user)
       invitation.accepted?.should be_true
     end
