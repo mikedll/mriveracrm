c1a91dd019b0bb0a6e65c9e7a2a745485dd10356
Mike De La Loza <mikedll@mikedll.com>
2015-01-08 00:22:51 -0800

    specs remove :current marker.

--- spec/models/usage_subscription_spec.rb
+++ spec/models/usage_subscription_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe UsageSubscription do
   context "validations" do
-    it "should require business", :current => true do
+    it "should require business" do
       @us = FactoryGirl.create(:usage_subscription)
       @us.business = nil
       @us.save.should be_false
