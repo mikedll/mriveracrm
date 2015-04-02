6e8732b81a56f60793e31ca155ac5a5f73c2e8d8
Mike De La Loza <mikedll@mikedll.com>
2015-04-02 06:46:01 -0700

    case 174 spec authorization to Manage business controller

---
+++ spec/controllers/manage/businesses_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Manage::BusinessesController do
+
+  context "authorization", :current => true do
+    before :each do
+      @user = FactoryGirl.create(:employee_user)
+      sign_in @user
+      request.host = @user.employee.business.host
+    end
+
+    it "should grant access to business owner" do
+      sign_in @user.employee.business.an_owner
+      get :show
+      response.should be_success
+    end
+
+    it "should deny access to non-owning employee user" do
+      get :show
+      response.should redirect_to business_path
+      response.should_not be_success
+      flash[:error].should == I18n.t('unauthorized.manage.business', :action => "manage")
+    end
+
+  end
+
+end
