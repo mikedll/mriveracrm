621734e37329ae6e2d7452f0164b22c81c1d90c8
Mike De La Loza <mikedll@mikedll.com>
2015-04-02 06:47:50 -0700

    case 174 spec authorization to Manage StatusMonitorController

--- spec/controllers/manage/status_monitor_spec.rb
+++ spec/controllers/manage/status_monitor_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe Manage::BusinessesController do
+describe Manage::StatusMonitorController do
 
-  context "authorization" do
+  context "authorization", :current => true do
     before :each do
       @user = FactoryGirl.create(:employee_user)
       sign_in @user
@@ -19,7 +19,7 @@ describe Manage::BusinessesController do
       get :show
       response.should redirect_to business_path
       response.should_not be_success
-      flash[:error].should == I18n.t('unauthorized.manage.business', :action => "manage")
+      flash[:error].should == I18n.t('unauthorized.read.business', :action => "read")
     end
 
   end
