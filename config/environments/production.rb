172ed2ff8c5f2740a74f5697a60f940c43c063f8
Mike De La Loza <mikedll@mikedll.com>
2015-04-03 01:39:45 -0700

    case 174 simplify SES default url host in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ MikedllCrm::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.mriveracrm.com:80' }
+  config.action_mailer.default_url_options = { :host => 'www.mriveracrm.com' }
   config.action_mailer.delivery_method = :ses
 
   config.action_controller.asset_host = 'd1dmxqf6tri91s.cloudfront.net'
