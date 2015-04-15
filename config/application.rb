c80c7a8c944d978ac39e068011ccf4487f81e8f2
Mike De La Loza <mikedll@mikedll.com>
2015-04-14 18:43:12 -0700

    case 238 add app/workers to autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 module MikedllCrm
   class Application < Rails::Application
 
-    config.autoload_paths += %W( #{Rails.root}/app/observers #{Rails.root}/app/uploaders )
+    config.autoload_paths += %W( #{Rails.root}/app/observers #{Rails.root}/app/uploaders #{Rails.root}/app/workers)
 
     config.time_zone = "Pacific Time (US & Canada)"
 
