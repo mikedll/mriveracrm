7864f2d95f0988684e0f493c552f857c302fe266
Mike De La Loza <mikedll@mikedll.com>
2015-04-14 18:50:55 -0700

    case 238 unique sql connection for resque job after fork

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,7 @@
 # Load the redis configuration from resque.yml
 Resque.redis = YAML.load_file(File.join(Rails.root, "config", "resque.yml"))[Rails.env.to_s]
+
+# Get own connection to PostgreSQL
+# https://devcenter.heroku.com/articles/forked-pg-connections#resque-ruby-queuing
+Resque.before_fork { ActiveRecord::Base.connection.disconnect! }
+Resque.after_fork { ActiveRecord::Base.establish_connection }
