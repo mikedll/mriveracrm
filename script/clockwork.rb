8c1002020169c9a9ae01fc487fd53d2c1358cb3a
Mike De La Loza <mikedll@mikedll.com>
2015-04-16 07:45:09 -0700

    case 238 realistic frequency for SEORanker.reset_windows!

--- script/clockwork.rb
+++ script/clockwork.rb
@@ -26,5 +26,5 @@ require 'app/workers/scheduled_event'
 
 module Clockwork
   handler { |job| Resque.enqueue_to(WorkerBase::Queues::DEFAULT, 'ScheduledEvent', job) }
-  every(10.seconds, ScheduledEvent::Events::RESET_SEO_RANKERS)
+  every(1.hour, ScheduledEvent::Events::RESET_SEO_RANKERS)
 end
