8908ca4df72a5b5ef55c1c49b02840807923ab59
Mike De La Loza <mikedll@mikedll.com>
2015-06-17 00:03:22 -0700

    simplified inline_label simple_form wrapper mapping for boolean inputs

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -120,7 +120,7 @@ SimpleForm.setup do |config|
 
   # Custom wrappers for input types. This should be a hash containing an input
   # type as key and the wrapper that will be used for all inputs with specified type.
-  # config.wrapper_mappings = { :string => :prepend }
+  config.wrapper_mappings = { :boolean => :inline_label }
 
   # Default priority for time_zone inputs.
   # config.time_zone_priority = nil
