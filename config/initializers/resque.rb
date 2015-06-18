aa0e760032527c68cf80e511f8979131a9c270ab
Mike De La Loza <mikedll@mikedll.com>
2015-06-18 01:20:21 -0700

    quieter resque. only logging :info and higher output.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,8 @@
 # Load the redis configuration from resque.yml
 Resque.redis = AppConfiguration.get('redis')
 
+Resque.logger.level = 1 # :debug, :info, :warn, :error, :fatal
+
 # Get own connection to PostgreSQL
 # https://devcenter.heroku.com/articles/forked-pg-connections#resque-ruby-queuing
 Resque.before_fork { ActiveRecord::Base.connection.disconnect! }
