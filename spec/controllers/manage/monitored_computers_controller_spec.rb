139b6c52a6262601c6dacd45f8a5cd524c315ac7
Mike De La Loza <mikedll@mikedll.com>
2015-08-08 02:06:25 -0700

    repair MonitoredComputersController spec for create

--- spec/controllers/manage/monitored_computers_controller_spec.rb
+++ spec/controllers/manage/monitored_computers_controller_spec.rb
@@ -12,8 +12,10 @@ describe Manage::MonitoredComputersController do
 
     it "should allow create" do
       @user.business.it_monitored_computers.count.should == 0
-      post :create, { :monitored_computer => { 'name' => "Special Client" } }
+      post :create, { :format => :json, 'name' => "Special Client", 'hostname' => 'crmdev.michaelriveraco.com' }
       @user.business.it_monitored_computers.count.should == 1
+      mc = @user.business.it_monitored_computers.first
+      mc.hostname.should == 'crmdev.michaelriveraco.com'
     end
   end
 
