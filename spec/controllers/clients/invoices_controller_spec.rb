71e46a3d6aab13744d47e95cd72613e71d9326af
Mike De La Loza <mikedll@mikedll.com>
2015-10-18 23:18:15 -0700

    removing :current marker from two specs in invoices_controller_spec

--- spec/controllers/clients/invoices_controller_spec.rb
+++ spec/controllers/clients/invoices_controller_spec.rb
@@ -12,19 +12,16 @@ describe Client::InvoicesController do
   end
 
   context "show" do
-    it "should show a given invoice", :current => true do
+    it "should show a given invoice" do
       get :show, :format => :json, :id => @invoice.id
       result = JSON.parse(response.body)
-      puts "*************** #{__FILE__} #{__LINE__} *************"
-      puts result.to_yaml
-
       result['id'].should == @invoice.id
     end
   end
 
   context "index" do
 
-    it "should present json results of invoices", :current => true do
+    it "should present json results of invoices" do
       get :index, :format => :json
       result = JSON.parse(response.body)
       result.length.should == 2
